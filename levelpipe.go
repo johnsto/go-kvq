b53ba7ba2792bbc049da048de7dc673a4803cfea
Dave Johnston <johnsto@gmail.com>
2014-10-29 21:08:32 +0000

    Added benchmarks

--- levelpipe.go
+++ levelpipe.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/binary"
 	"log"
+	"sort"
 	"sync"
 
 	"github.com/jmhodges/levigo"
@@ -10,6 +11,18 @@ import (
 )
 
 var snow *gosnow.SnowFlake
+
+type Uint64Slice []uint64
+
+func (p Uint64Slice) Len() int           { return len(p) }
+func (p Uint64Slice) Less(i, j int) bool { return p[i] < p[j] }
+func (p Uint64Slice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p Uint64Slice) Sort()              { sort.Sort(p) }
+func (p Uint64Slice) Search(v uint64) int {
+	return sort.Search(len(p), func(i int) bool {
+		return p[i] == v
+	})
+}
 
 func init() {
 	var err error
@@ -23,25 +36,72 @@ type Pipe struct {
 	db    *levigo.DB
 	taken map[uint64]struct{}
 	mutex *sync.Mutex
-	start []byte
-	it    *levigo.Iterator
+	keys  []uint64
+	first uint64
 }
 
+// DestroyPipe destroys the pipe at the given path.
+func DestroyPipe(path string) error {
+	return levigo.DestroyDatabase(path, levigo.NewOptions())
+}
+
+// NewPipe creates a new pipe at the given path.
 func NewPipe(path string) (*Pipe, error) {
+	return open(path, true)
+}
+
+// OpenPipe opens the pipe at the given path.
+func OpenPipe(path string) (*Pipe, error) {
+	return open(path, false)
+}
+
+func open(path string, create bool) (*Pipe, error) {
 	opts := levigo.NewOptions()
-	opts.SetCreateIfMissing(true)
+	opts.SetCreateIfMissing(create)
 	db, err := levigo.Open(path, opts)
 	if err != nil {
 		return nil, err
 	}
-	ro := levigo.NewReadOptions()
-
-	return &Pipe{
+	pipe := &Pipe{
 		db:    db,
 		taken: make(map[uint64]struct{}),
 		mutex: &sync.Mutex{},
-		it:    db.NewIterator(ro),
-	}, nil
+		keys:  make([]uint64, 0),
+	}
+	pipe.init()
+	return pipe, nil
+}
+
+func (p *Pipe) init() {
+	ro := levigo.NewReadOptions()
+	defer ro.Close()
+	it := p.db.NewIterator(ro)
+	defer it.Close()
+	it.SeekToFirst()
+	if it.Valid() {
+		id, n := binary.Uvarint(it.Key())
+		if n <= 0 {
+			log.Fatalln("couldn't parse key to ID", it.Key())
+		}
+		p.keys = append(p.keys, id)
+	}
+	Uint64Slice(p.keys).Sort()
+}
+
+func (p *Pipe) findFirstKey() uint64 {
+	ro := levigo.NewReadOptions()
+	defer ro.Close()
+	it := p.db.NewIterator(ro)
+	defer it.Close()
+	it.SeekToFirst()
+	if it.Valid() {
+		id, n := binary.Uvarint(it.Key())
+		if n <= 0 {
+			log.Fatalln("couldn't parse key to ID", it.Key())
+		}
+		return id
+	}
+	return 0
 }
 
 func (p *Pipe) Close() {
@@ -52,41 +112,80 @@ func (p *Pipe) Put() Put {
 	return Put{
 		p:     p,
 		b:     levigo.NewWriteBatch(),
+		mutex: &sync.Mutex{},
 		puts:  make(map[uint64]struct{}),
 		first: 0,
 	}
 }
 
+func (p *Pipe) put(ids ...uint64) {
+	p.keys = append(p.keys, ids...)
+	Uint64Slice(p.keys).Sort()
+}
+
+func (p *Pipe) take(ids ...uint64) {
+	for _, id := range ids {
+		pos := Uint64Slice(p.keys).Search(id)
+		if len(p.keys) > pos {
+			p.keys = append(p.keys[:pos], p.keys[pos+1:]...)
+		} else {
+			p.keys = p.keys[:pos]
+		}
+	}
+}
+
 func (p *Pipe) Take() Take {
+	ro := levigo.NewReadOptions()
 	return Take{
 		p:     p,
 		b:     levigo.NewWriteBatch(),
 		taken: make(map[uint64]struct{}),
 		mutex: &sync.Mutex{},
+		it:    p.db.NewIterator(ro),
 	}
 }
 
-func (p *Pipe) take() (k, v []byte, err error) {
-	return nil, nil, nil
+func (p *Pipe) Clear() error {
+	ro := levigo.NewReadOptions()
+	defer ro.Close()
+	b := levigo.NewWriteBatch()
+	it := p.db.NewIterator(ro)
+	it.SeekToFirst()
+	for it.Valid() {
+		b.Delete(it.Key())
+	}
+	wo := levigo.NewWriteOptions()
+	defer wo.Close()
+	wo.SetSync(true)
+	return p.db.Write(wo, b)
 }
 
-func (p *Pipe) mark(k []byte) {
+// firstKey returns the first available key for taking
+func (p *Pipe) firstKey() []byte {
+	for _, id := range p.keys {
+		if _, ok := p.taken[id]; !ok {
+			k := make([]byte, 16)
+			if binary.PutUvarint(k, id) <= 0 {
+				log.Fatalln("couldn't write key")
+			}
+			return k
+		}
+	}
+	return nil
+}
+
+// mark sets the 'taken' flag for an entry ID, preventing further takes.
+func (p *Pipe) mark(k uint64) {
 	p.mutex.Lock()
 	defer p.mutex.Unlock()
-	id, err := binary.Uvarint(k)
-	if err != nil {
-		log.Fatalln("couldn't translate key to uint64:", err)
-	}
-	p.taken[id] = struct{}{}
+	p.taken[k] = struct{}{}
 }
 
-func (p *Pipe) unmark(k []byte) {
+// unmark clears the 'taken' flag for an entry ID, making it available to be
+// taken.
+func (p *Pipe) unmark(id uint64) {
 	p.mutex.Lock()
 	defer p.mutex.Unlock()
-	id, err := binary.Uvarint(k)
-	if err != nil {
-		log.Fatalln("couldn't translate key to uint64:", err)
-	}
 	delete(p.taken, id)
 }
 
@@ -105,6 +204,9 @@ func (p *Put) Put(v []byte) error {
 		return err
 	}
 	k := make([]byte, 16)
+	if binary.PutUvarint(k, id) <= 0 {
+		log.Fatalln("couldn't write key")
+	}
 	p.b.Put(k, v)
 	p.mutex.Lock()
 	defer p.mutex.Unlock()
@@ -124,7 +226,11 @@ func (p *Put) Close() {
 func (p *Put) Commit() error {
 	wo := levigo.NewWriteOptions()
 	defer wo.Close()
+	wo.SetSync(true)
 	err := p.p.db.Write(wo, p.b)
+	for id := range p.puts {
+		p.p.put(id)
+	}
 	return err
 }
 
@@ -139,10 +245,35 @@ type Take struct {
 	b     *levigo.WriteBatch
 	taken map[uint64]struct{}
 	mutex *sync.Mutex
+	it    *levigo.Iterator
 }
 
 // Take gets an item from the pipe.
 func (t *Take) Take() ([]byte, error) {
+	t.mutex.Lock()
+	defer t.mutex.Unlock()
+	if firstKey := t.p.firstKey(); firstKey != nil {
+		t.it.Seek(firstKey)
+	} else {
+		t.it.SeekToFirst()
+	}
+	m := 0
+	for t.it.Valid() {
+		m++
+		k := t.it.Key()
+		v := t.it.Value()
+		id, n := binary.Uvarint(k)
+		if n <= 0 {
+			log.Fatalln("couldn't parse key: " + string(k))
+		}
+		if _, ok := t.p.taken[id]; !ok {
+			t.p.mark(id)
+			t.b.Delete(k)
+			return v, nil
+		}
+		t.taken[id] = struct{}{}
+		t.it.Next()
+	}
 	return nil, nil
 }
 
@@ -150,17 +281,20 @@ func (t *Take) Take() ([]byte, error) {
 func (t *Take) Commit() error {
 	wo := levigo.NewWriteOptions()
 	defer wo.Close()
+	wo.SetSync(true)
 	err := t.p.db.Write(wo, t.b)
 	for k := range t.taken {
-		t.p.Unflag(k)
+		t.p.unmark(k)
+		t.p.take(k)
 	}
+	t.taken = make(map[uint64]struct{})
 	return err
 }
 
 // Rollback returns the items to the queue.
 func (t *Take) Rollback() error {
 	for k := range t.taken {
-		t.p.Unflag(k)
+		t.p.unmark(k)
 	}
 	return nil
 }
--- levelpipe_test.go
+++ levelpipe_test.go
@@ -2,34 +2,154 @@ package main
 
 import (
 	"log"
+	"strconv"
+	"sync"
 	"testing"
 
-	"github.com/coreos/etcd/third_party/github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 )
 
-func Test_Pipe(t *testing.T) {
+func TestPipe(t *testing.T) {
+	err := DestroyPipe("pipe.db")
 	p, err := NewPipe("pipe.db")
 	if err != nil {
 		log.Fatalln(err)
 	}
+	defer func() {
+		p.Close()
+	}()
+	if err := p.Clear(); err != nil {
+		log.Fatalln(err)
+	}
+
+	keys := make(map[string]bool)
 
 	tx := p.Put()
-	tx.Put([]byte("e"))
-	tx.Put([]byte("a"))
-	tx.Put([]byte("f"))
-	tx.Put([]byte("c"))
-	tx.Put([]byte("b"))
-	tx.Put([]byte("d"))
+	for i := 0; i < 100; i++ {
+		s := strconv.Itoa(i)
+		tx.Put([]byte(s))
+		keys[s] = true
+	}
 	tx.Commit()
 
 	rx := p.Take()
-	take := func() []byte {
+	for len(keys) != 0 {
 		v, err := rx.Take()
 		assert.NoError(t, err)
-		return v
+		s := string(v)
+		assert.True(t, keys[s])
+		delete(keys, s)
 	}
-	assert.Equal(t, []byte("a"), take())
-	assert.Equal(t, []byte("b"), take())
-	assert.Equal(t, []byte("c"), take())
 	rx.Commit()
+
+	// Verify there are no more items in the queue
+	rx = p.Take()
+	v, err := rx.Take()
+	assert.Nil(t, err)
+	assert.Nil(t, v)
+}
+
+func TestPipeMulti(t *testing.T) {
+	err := DestroyPipe("pipe.db")
+	p, err := NewPipe("pipe.db")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer func() {
+		p.Close()
+	}()
+	if err := p.Clear(); err != nil {
+		log.Fatalln(err)
+	}
+
+	inDone := false
+	inGroup := &sync.WaitGroup{}
+	outGroup := &sync.WaitGroup{}
+
+	for i := 0; i < 5; i++ {
+		inGroup.Add(1)
+		go func() {
+			tx := p.Put()
+			for i := 0; i < 100; i++ {
+				s := strconv.Itoa(i)
+				err := tx.Put([]byte(s))
+				assert.Nil(t, err)
+			}
+			tx.Commit()
+			inGroup.Done()
+		}()
+	}
+
+	for i := 0; i < 5; i++ {
+		outGroup.Add(1)
+		go func() {
+			for {
+				rx := p.Take()
+				v, err := rx.Take()
+				assert.Nil(t, err)
+				if inDone && v == nil {
+					break
+				}
+				rx.Commit()
+			}
+			outGroup.Done()
+		}()
+	}
+
+	inGroup.Wait()
+	inDone = true
+	outGroup.Wait()
+
+	// Verify there are no more items in the queue
+	rx := p.Take()
+	v, err := rx.Take()
+	assert.Nil(t, err)
+	assert.Nil(t, v)
+}
+
+func BenchmarkWrite(b *testing.B) {
+	DestroyPipe("benchmark.db")
+	p, err := NewPipe("benchmark.db")
+	assert.Nil(b, err)
+	defer func() {
+		p.Close()
+	}()
+	b.ResetTimer()
+	tx := p.Put()
+	for i := 0; i < b.N; i++ {
+		s := strconv.Itoa(i)
+		tx.Put([]byte(s))
+	}
+	tx.Commit()
+	b.StopTimer()
+}
+
+func BenchmarkRead(b *testing.B) {
+	DestroyPipe("benchmark.db")
+	p, err := NewPipe("benchmark.db")
+	assert.Nil(b, err)
+	defer func() {
+		p.Close()
+	}()
+	tx := p.Put()
+	for i := 0; i < b.N; i++ {
+		s := strconv.Itoa(i)
+		if err := tx.Put([]byte(s)); err != nil {
+			b.Fatal("error during put:", err)
+		}
+	}
+	tx.Commit()
+	b.ResetTimer()
+	rx := p.Take()
+	for {
+		v, err := rx.Take()
+		if err != nil {
+			b.Fatal("error during take:", err)
+		}
+		if len(v) == 0 {
+			break
+		}
+	}
+	rx.Commit()
+	b.StopTimer()
 }
