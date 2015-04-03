fe798dc31e247da1e190e1c8db682b33de2d78b1
Dave Johnston <johnsto@gmail.com>
2015-04-03 15:37:33 +0000

    Update package doc

--- doc.go
+++ doc.go
@@ -1,13 +1,13 @@
 /*
 
-Package leviq provides a transactional, LevelDB-backed queue, built on top of
-levigo (https://github.com/jmhodges/levigo) and gosnow (https://github.com/sdming/gosnow).
+Package leviq is a persistent transactional queue. It supports goleveldb, levigo and Bolt as backends.
 
-leviq.Open opens and creates queues.
+To open a queue, first get a database instance, and then the queue itself. A DB can contain many queues of different names.
 
-	q, err := leviq.Open("queue.db", nil)
+	db, err := goleveldb.Open("db.db")
+	queue, err := db.Queue("tweets")
 
-(Remember to Close() your queue when finished with it.)
+(Remember to Close() when finished)
 
 To push data onto a queue, start a transaction, add values, and then commit.
 
@@ -27,28 +27,17 @@ To take data from the queue, start a transaction, take values, and the commit.
 Items on the queue are keyed by the time at which they were Put(), so the push
 order should generally match the pop order (barring any run-time pauses).
 
+To retrieve a certain number of items within a timeframe, use Txn.TakeN:
+
+	values, err := t.TakeN(100, 5 * time.Second)
+
+This will take up to 100 items within 5 seconds (total), and return the values
+when either of those conditions are met.
+
 Data in transactions are only persisted when Commit() is called. Any items
 put in a transaction do not become available for taking until the transaction
 is committed. Similarly, any items taken in a transaction are made available
 again if the transaction is closed without being committed.
-
-By default, LevelDB does not flush data to disk before returning - so data
-can be lost even after Commit() has been called. To decrease the chances of
-data loss, enable sync:
-
-	q, err := leviq.Open("queue.db", nil)
-	q.SetSync(true)
-
-Note that enabling sync drastically decreases write performance.
-
-For an indication of read/write performance, try running the included
-benchmarks (note the disparity between Sync and non-Sync write performance).
-
-	GOMAXPROCS=4 go test -bench=.
-
-The benchmarks are set up such that each Put or Take constitutes a single 'op'
-in Go's benchmarking parlance, therefore making it easy to convert to a very
-rough events/sec measurement.
 
 */
 package leviq // import "github.com/johnsto/leviq"
